3464b5d9bdc14b91ce6b4de8f6dbf53eb44d4ac3
Guza xt <gustavomom1227@gmail.com>
2023-04-04 14:29:47 +0000

    save

---
+++ Cliente.java
@@ -0,0 +1,8 @@
+public class Cliente {
+    public static void main(String[] args){
+        Conta conta=new Conta();
+        conta.setNumero(4358);
+
+        System.out.println(conta.getNumero());
+    }    
+}
